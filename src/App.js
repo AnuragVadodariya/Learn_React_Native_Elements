722cb6370cfad8b96809153d66e9e290ab8acc68
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-16 11:08:03 +0530

    Divider

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { CheckBox } from "react-native-elements";
+import { Divider } from "react-native-elements";
 
 const App = () => {
   return (
     <View style={styles.app}>
       <View style={styles.header}>
-        <CheckBox title="Click Here" />
-
-        <CheckBox center title="Click Here" />
-
-        <CheckBox
-          center
-          title="Click Here"
-          checkedIcon="dot-circle-o"
-          uncheckedIcon="circle-o"
-        />
-
-        <CheckBox
-          center
-          title="Click Here to Remove This Item"
-          iconRight
-          iconType="material"
-          checkedIcon="clear"
-          uncheckedIcon="add"
-          checkedColor="red"
-        />
+        <Divider style={{ backgroundColor: "blue" }} />;
       </View>
     </View>
   );
