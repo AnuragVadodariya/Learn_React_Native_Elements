5b1494f545575c988779091db92814a161238a0d
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-16 13:18:13 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
-import { AirbnbRating } from "react-native-ratings";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { View, StyleSheet } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.app}>
       <View style={styles.header}>
-        <TouchableOpacity>
-          <AirbnbRating
-            count={11}
-            reviews={[
-              "Terrible",
-              "Bad",
-              "Meh",
-              "OK",
-              "Good",
-              "Hmm...",
-              "Very Good",
-              "Wow",
-              "Amazing",
-              "Unbelievable",
-              "Jesus"
-            ]}
-            defaultRating={11}
-            size={20}
-          />
-        </TouchableOpacity>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={() => console.log("user search")}
+          value=""
+        />
       </View>
     </View>
   );
