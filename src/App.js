37b9e9c97af6ed27d1021ed3758e6ce50ba972b7
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-16 10:36:31 +0530

    Button

--- src/App.js
+++ src/App.js
@@ -1,61 +1,18 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { Avatar, Badge } from "react-native-elements";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-elements";
 
 const App = () => {
   return (
     <View style={styles.app}>
       <View style={styles.header}>
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          value="99+"
-          status="error"
-        />
-
-        <Badge
-          style={{ padding: 20, margin: 20, cursor: "pointer" }}
-          value={<Text>My Custom Badge</Text>}
-        />
-
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          status="success"
-          value="12+"
-          size={40}
-        />
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          status="error"
-          value="2+"
-          size={40}
-        />
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          status="primary"
-          value="03+"
-          size={40}
-        />
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          status="warning"
-          value="55+"
-          size={40}
-        />
-
-        <Avatar
-          rounded
-          source={{
-            uri: "https://randomuser.me/api/portraits/men/41.jpg"
-          }}
-          size="large"
-        />
-
-        <Badge
-          style={{ marginHorizontal: 20, cursor: "pointer" }}
-          status="success"
-          value="9+"
-          containerStyle={{ position: "absolute", top: 20, right: 17 }}
-        />
+        <Button title="Read More" style={{ marginBottom: 20 }} />
+        <Button title="More" style={{ marginBottom: 20 }} type="outline" />
+        <Button title="Read More" style={{ marginBottom: 20 }} />
+        <Button title=" More" style={{ marginBottom: 20 }} type="clear" />
+        <Button title="Read More" style={{ marginBottom: 20 }} />
+        <Button title="Read More" style={{ marginBottom: 20 }} type="outline" />
+        <Button title=" More" style={{ marginBottom: 20 }} type="outline" />
       </View>
     </View>
   );
@@ -68,7 +25,7 @@ const styles = StyleSheet.create({
   },
   header: {
     padding: 20,
-    flexDirection: "row"
+    flexDirection: "column"
   },
   code: {
     fontFamily: "monospace, monospace"
