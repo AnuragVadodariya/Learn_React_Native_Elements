0524c6eb3c3a013413909d31727ec44e94081a07
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-16 13:23:51 +0530

    Social Icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
-import { SearchBar } from "react-native-elements";
+import { SocialIcon } from "react-native-elements";
 import { View, StyleSheet } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.app}>
       <View style={styles.header}>
-        <SearchBar
-          placeholder="Type Here..."
-          onChangeText={() => console.log("user search")}
-          value=""
+        {/* icons */}
+        <SocialIcon type="twitter" />
+        <SocialIcon raised={false} type="gitlab" />
+        <SocialIcon light type="medium" />
+        <SocialIcon light raised={false} type="medium" />
+        {/* Button */}
+        <SocialIcon
+          title="Sign In With Facebook"
+          button
+          type="facebook"
+          style={{ textAlign: "center" }}
+        />
+        <SocialIcon
+          title="Some Twitter Message"
+          button
+          type="twitter"
+          style={{ textAlign: "center" }}
+        />
+        <SocialIcon button type="medium" style={{ textAlign: "center" }} />
+        <SocialIcon
+          button
+          light
+          type="instagram"
+          style={{ textAlign: "center" }}
         />
       </View>
     </View>
