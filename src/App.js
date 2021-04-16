2b6425450fceb9d1b2004258aa750e987ac21696
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-16 09:58:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Button } from "react-native-elements";
+import { Avatar } from "react-native-elements";
 
 const App = () => {
   return (
     <View style={styles.app}>
       <View style={styles.header}>
-        <Button raised title="Click Here !!" titleStyle={{ borderRadius: 7 }} />
+        <Avatar
+          activeOpacity={0.5}
+          size="60"
+          icon={{ name: "home", color: "black" }}
+          style={{ border: 3 }}
+        />
+        <Avatar rounded title="AV" />
+        <Avatar
+          size="small"
+          rounded
+          onPress={() => alert("It's Work")}
+          title="ABC"
+        />
+        <Avatar
+          size="small"
+          rounded
+          title="XYZ"
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
       </View>
     </View>
   );
@@ -18,7 +37,8 @@ const styles = StyleSheet.create({
     maxWidth: 500
   },
   header: {
-    padding: 20
+    padding: 20,
+    flexDirection: "column"
   },
   code: {
     fontFamily: "monospace, monospace"
